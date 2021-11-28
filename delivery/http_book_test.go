b0f5a2320e2547c21ed3b70aac77ff995209c6de
winarto <winarto@intern.bukalapak.com>
2021-11-28 11:08:12 +0700

    adding unit testing for delivery

---
+++ delivery/http_book_test.go
@@ -0,0 +1,261 @@
+package delivery_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	bookstorebe "winartodev/book-store-be"
+	"winartodev/book-store-be/delivery"
+	"winartodev/book-store-be/handler"
+	"winartodev/book-store-be/mocks"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func newBookHandler() (http.Handler, *mocks.BookUsecase) {
+	uc := new(mocks.BookUsecase)
+	book := delivery.NewBookHandler(uc)
+	h := handler.NewHandler(&book)
+	return h, uc
+}
+
+func TestGetBooks(t *testing.T) {
+	testCases := []struct {
+		name     string
+		books    []bookstorebe.Book
+		wantErr  bool
+		getError error
+	}{
+		{
+			name: "success",
+			books: []bookstorebe.Book{
+				{
+					ID:          1,
+					PublisherID: 1,
+					CategoryID:  1,
+					Title:       "Clean Architecture: A Craftsman's Guide to Software Structure and Design",
+					Author:      "Robert C. Martin",
+					Publication: 2020,
+					Stock:       10,
+				},
+			},
+		},
+		{
+			name:  "success with no data",
+			books: []bookstorebe.Book{},
+		},
+		{
+			name:     "failed to get books",
+			books:    []bookstorebe.Book{},
+			wantErr:  true,
+			getError: errors.New("failed to get books"),
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			handler, book := newBookHandler()
+			book.On("GetBooks", mock.Anything).Return(test.books, test.getError)
+
+			recoder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodGet, "/bookstore/book", nil)
+
+			handler.ServeHTTP(recoder, request)
+
+			assert.Equal(t, test.wantErr, recoder.Code != http.StatusOK)
+		})
+	}
+}
+
+func TestGetBook(t *testing.T) {
+	testCases := []struct {
+		name    string
+		id      int64
+		book    bookstorebe.Book
+		wantErr bool
+		getErr  error
+	}{
+		{
+			name: "success",
+			id:   1,
+			book: bookstorebe.Book{
+				ID:          1,
+				PublisherID: 1,
+				CategoryID:  1,
+				Title:       "Clean Architecture: A Craftsman's Guide to Software Structure and Design",
+				Author:      "Robert C. Martin",
+				Publication: 2020,
+				Stock:       10,
+			},
+		},
+		{
+			name:    "success with book not found",
+			id:      1,
+			book:    bookstorebe.Book{},
+			wantErr: true,
+		},
+		{
+			name:    "failed to get book data",
+			id:      1,
+			book:    bookstorebe.Book{},
+			wantErr: true,
+			getErr:  errors.New("failed to get book data"),
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			handler, book := newBookHandler()
+			book.On("GetBook", mock.Anything, mock.Anything).Return(test.book, test.getErr)
+
+			recoder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/bookstore/book/%d", test.id), nil)
+
+			handler.ServeHTTP(recoder, request)
+
+			assert.Equal(t, test.wantErr, recoder.Code != http.StatusOK)
+		})
+	}
+}
+
+func TestCreateBook(t *testing.T) {
+	testCases := []struct {
+		name      string
+		book      bookstorebe.Book
+		wantErr   bool
+		createErr error
+	}{
+		{
+			name: "success",
+			book: bookstorebe.Book{
+				ID:          1,
+				PublisherID: 1,
+				CategoryID:  1,
+				Title:       "Clean Architecture: A Craftsman's Guide to Software Structure and Design",
+				Author:      "Robert C. Martin",
+				Publication: 2020,
+				Stock:       10,
+			},
+		},
+		{
+			name: "failed create book",
+			book: bookstorebe.Book{
+				ID:          1,
+				PublisherID: 1,
+				CategoryID:  1,
+				Title:       "Clean Architecture: A Craftsman's Guide to Software Structure and Design",
+				Author:      "Robert C. Martin",
+				Publication: 2020,
+				Stock:       10,
+			},
+			wantErr:   true,
+			createErr: errors.New("failed create book"),
+		},
+	}
+
+	for _, test := range testCases {
+		handler, book := newBookHandler()
+		book.On("CreateBook", mock.Anything, mock.Anything).Return(test.createErr)
+
+		body, _ := json.Marshal(test.book)
+		recoder := httptest.NewRecorder()
+		requset := httptest.NewRequest(http.MethodPost, "/bookstore/book", bytes.NewBuffer(body))
+
+		handler.ServeHTTP(recoder, requset)
+
+		assert.Equal(t, test.wantErr, recoder.Code != http.StatusCreated)
+	}
+}
+
+func TestUpdateBook(t *testing.T) {
+	testCases := []struct {
+		name      string
+		id        int64
+		book      bookstorebe.Book
+		wantErr   bool
+		updateErr error
+	}{
+		{
+			name: "success",
+			id:   1,
+			book: bookstorebe.Book{
+				ID:          1,
+				PublisherID: 1,
+				CategoryID:  1,
+				Title:       "Clean Architecture: A Craftsman's Guide to Software Structure and Design",
+				Author:      "Robert C. Martin",
+				Publication: 2020,
+				Stock:       10,
+			},
+		},
+		{
+			name: "failed to create book",
+			id:   1,
+			book: bookstorebe.Book{
+				ID:          1,
+				PublisherID: 1,
+				CategoryID:  1,
+				Title:       "Clean Architecture: A Craftsman's Guide to Software Structure and Design",
+				Author:      "Robert C. Martin",
+				Publication: 2020,
+				Stock:       10,
+			},
+			wantErr:   true,
+			updateErr: errors.New("failed to create book"),
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			handler, book := newBookHandler()
+			book.On("UpdateBook", mock.Anything, mock.Anything, mock.Anything).Return(test.updateErr)
+
+			body, _ := json.Marshal(test.book)
+			recoder := httptest.NewRecorder()
+			requset := httptest.NewRequest(http.MethodPut, fmt.Sprintf("/bookstore/book/%d", test.id), bytes.NewBuffer(body))
+
+			handler.ServeHTTP(recoder, requset)
+
+			assert.Equal(t, test.wantErr, recoder.Code != http.StatusOK)
+		})
+	}
+}
+
+func TestDeleteBook(t *testing.T) {
+	testCases := []struct {
+		name      string
+		id        int64
+		wantError bool
+		deleteErr error
+	}{
+		{
+			name: "success",
+			id:   1,
+		},
+		{
+			name:      "failed delete data",
+			id:        1,
+			wantError: true,
+			deleteErr: errors.New("failed delete data"),
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			handler, book := newBookHandler()
+			book.On("DeleteBook", mock.Anything, mock.Anything).Return(test.deleteErr)
+
+			recoder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/bookstore/book/%d", test.id), nil)
+
+			handler.ServeHTTP(recoder, request)
+
+			assert.Equal(t, test.wantError, recoder.Code != http.StatusOK)
+		})
+	}
+}
---
+++ delivery/http_category_test.go
@@ -0,0 +1,229 @@
+package delivery_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	bookstorebe "winartodev/book-store-be"
+	"winartodev/book-store-be/delivery"
+	"winartodev/book-store-be/handler"
+	"winartodev/book-store-be/mocks"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func newCategoryHandler() (http.Handler, *mocks.CategoryUsecase) {
+	uc := new(mocks.CategoryUsecase)
+	category := delivery.NewCategoryHandler(uc)
+	h := handler.NewHandler(&category)
+	return h, uc
+}
+
+func TestGetCategories(t *testing.T) {
+	testCases := []struct {
+		name      string
+		endpoint  string
+		category  []bookstorebe.Category
+		wantError bool
+		getError  error
+	}{
+		{
+			name:      "success",
+			endpoint:  "/bookstore/category",
+			category:  []bookstorebe.Category{{ID: 1, Name: "Classics"}, {ID: 2, Name: "Detective and Mystery"}},
+			wantError: false,
+			getError:  nil,
+		},
+		{
+			name:      "success with no data",
+			endpoint:  "/bookstore/category",
+			category:  []bookstorebe.Category{},
+			wantError: false,
+			getError:  nil,
+		},
+		{
+			name:      "failed get category data",
+			endpoint:  "/bookstore/category",
+			category:  []bookstorebe.Category{},
+			wantError: true,
+			getError:  errors.New("failed get category data"),
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			handler, category := newCategoryHandler()
+			category.On("GetCategories", mock.Anything).Return(test.category, test.getError)
+
+			recoder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodGet, test.endpoint, nil)
+			handler.ServeHTTP(recoder, request)
+
+			assert.Equal(t, test.wantError, recoder.Code != http.StatusOK)
+		})
+	}
+}
+
+func TestGetCategory(t *testing.T) {
+	testCases := []struct {
+		name     string
+		id       int64
+		category bookstorebe.Category
+		wantErr  bool
+		getError error
+	}{
+		{
+			name:     "success",
+			id:       1,
+			category: bookstorebe.Category{ID: 1, Name: "Classics"},
+			wantErr:  false,
+			getError: nil,
+		},
+		{
+			name:     "failed with category not found",
+			id:       1,
+			category: bookstorebe.Category{},
+			wantErr:  true,
+			getError: nil,
+		},
+		{
+			name:     "failed to get category",
+			id:       1,
+			category: bookstorebe.Category{ID: 1, Name: "Classics"},
+			wantErr:  true,
+			getError: errors.New("failed to get category"),
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			handler, category := newCategoryHandler()
+			category.On("GetCategory", mock.Anything, mock.Anything).Return(test.category, test.getError)
+
+			recoder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/bookstore/category/%v", test.id), nil)
+			handler.ServeHTTP(recoder, request)
+
+			assert.Equal(t, test.wantErr, recoder.Code != http.StatusOK)
+		})
+	}
+}
+
+func TestCreateCategory(t *testing.T) {
+	testCases := []struct {
+		name      string
+		endpoint  string
+		category  bookstorebe.Category
+		wantError bool
+		createErr error
+	}{
+		{
+			name:      "success",
+			endpoint:  "/bookstore/category",
+			category:  bookstorebe.Category{ID: 1, Name: "Classics"},
+			wantError: false,
+			createErr: nil,
+		},
+		{
+			name:      "failed created category",
+			endpoint:  "/bookstore/category",
+			category:  bookstorebe.Category{ID: 1, Name: "Classics"},
+			wantError: true,
+			createErr: errors.New("failed created category"),
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			handler, category := newCategoryHandler()
+			category.On("CreateCategory", mock.Anything, mock.Anything).Return(test.createErr)
+
+			body, _ := json.Marshal(test.category)
+			recoder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodPost, test.endpoint, bytes.NewBuffer(body))
+
+			handler.ServeHTTP(recoder, request)
+			assert.Equal(t, test.wantError, recoder.Code != http.StatusCreated)
+		})
+	}
+}
+
+func TestUpdateCategory(t *testing.T) {
+	testcases := []struct {
+		name      string
+		id        int64
+		category  bookstorebe.Category
+		wantError bool
+		updateErr error
+	}{
+		{
+			name:      "success",
+			id:        1,
+			category:  bookstorebe.Category{ID: 1, Name: "Classics"},
+			wantError: false,
+			updateErr: nil,
+		},
+		{
+			name:      "failed update category",
+			id:        1,
+			category:  bookstorebe.Category{ID: 1, Name: "Classics"},
+			wantError: true,
+			updateErr: errors.New("fail update category"),
+		},
+	}
+
+	for _, test := range testcases {
+		t.Run(test.name, func(t *testing.T) {
+			handler, category := newCategoryHandler()
+			category.On("UpdateCategory", mock.Anything, mock.Anything, mock.Anything).Return(test.updateErr)
+
+			body, _ := json.Marshal(test.category)
+
+			recoder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodPut, fmt.Sprintf("/bookstore/category/%d", test.id), bytes.NewBuffer(body))
+			handler.ServeHTTP(recoder, request)
+
+			assert.Equal(t, test.wantError, recoder.Code != http.StatusOK)
+		})
+	}
+}
+
+func TestDeleteCategory(t *testing.T) {
+	testcases := []struct {
+		name      string
+		id        int64
+		wantErr   bool
+		deleteErr error
+	}{
+		{
+			name:      "success",
+			id:        1,
+			wantErr:   false,
+			deleteErr: nil,
+		},
+		{
+			name:      "failed to delete category",
+			id:        1,
+			wantErr:   true,
+			deleteErr: errors.New("failed to delete category"),
+		},
+	}
+
+	for _, test := range testcases {
+		t.Run(test.name, func(t *testing.T) {
+			handler, category := newCategoryHandler()
+			category.On("DeleteCategory", mock.Anything, mock.Anything).Return(test.deleteErr)
+
+			recoder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/bookstore/category/%d", test.id), nil)
+			handler.ServeHTTP(recoder, request)
+
+			assert.Equal(t, test.wantErr, recoder.Code != http.StatusOK)
+		})
+	}
+}
